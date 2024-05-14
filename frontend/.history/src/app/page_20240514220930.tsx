d88e2411a3d0c8e7e2fe6fb5c490274dc4e753fa
VhalennnG <fanydanny237@gmail.com>
2024-05-15 01:12:14 +0800

    first up

---
+++ frontend/.history/src/app/page_20240514220930.tsx
@@ -0,0 +1,23 @@
+import { Button } from "@/components/ui/button";
+
+async function getStrapiData(path: string) {
+  const baseUrl = "http://127.0.0.1:1337";
+  try {
+    const response = await fetch(baseUrl + path);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export default async function Home() {
+  const strapiData = await getStrapiData("/api/home-page");
+  console.log(strapiData);
+
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <Button>Click Me</Button>
+    </main>
+  );
+}
